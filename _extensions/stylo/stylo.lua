fd1c944ecae596cbf7939f83b78affa3da8d6870
Nicolas Sauret <nnicolaoss@gmail.com>
2023-06-20 14:39:00 +0200

    tentative gestion author via lua puis tentative python filter

--- _extensions/stylo/stylo.lua
+++ _extensions/stylo/stylo.lua
@@ -3,6 +3,25 @@
 function Meta(m)
     if m.title_f then
       m.title = m.title_f
-      return m
-    end
+
+-- -- Create a new list, author, with modified structure
+--     m.author = {}
+--     for _, coucou in ipairs(m.coucous) do
+--         local forname = tostring(coucou.forname[1])
+--         local surname = tostring(coucou.surname[1])
+--         coucou.name = coucou.forname[0] .. " " .. coucou.surname[0]
+--         -- Create a new entry with the modified structure
+--         local newEntry = {
+--             biography = coucou.biography,
+--             name = coucou.name  
+--         }
+
+--     -- Add the new entry to the author list
+--     table.insert(m.author, newEntry)
+--     end
+
+    -- -- Convert the modified table back to a YAML string
+    -- local modifiedYamlString = lyaml.dump(yamlTable)
+
+    return m
   end
